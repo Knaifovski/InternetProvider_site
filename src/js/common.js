e3286d357c2f294575ed75ee9e4ba3156365a70b
NickolaQ Knaifovski <31153601+Knaifovski@users.noreply.github.com>
2018-04-03 01:02:44 +0300

    Add files via upload

---
+++ src/js/common.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+
+	$('.owl-carousel').owlCarousel({
+    margin:0,
+    loop:true,
+    //dots:true,
+    autoplay:true,
+    autoplayTimeout:3000,
+    items:1,
+    //nav: true
+	});
+});
